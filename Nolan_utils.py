8c5ae597adaa8a5ed56d5c9f205a7b6a7c402790
Nolan Moss <140113152+Crusoe22@users.noreply.github.com>
2024-01-10 20:42:46 -0500

    Update Nolan_utils.py

--- Nolan_utils.py
+++ Nolan_utils.py
@@ -3,36 +3,33 @@
 import math
 import statistics
 
-''' Define at least one variable of each of these types: str, int, float, bool, list of strings, list of numbers. The following example
-uses optional type hints. Follow naming conventions for variable names, but create your own names and values. Note boolean values are typically named as a question, e.g., has_international_presence.'''
-
 
 company_name: str = "Nolan's European Government Analytics"
-current_customers: int = 67
-trades_with_united_states: bool = True
+current_customers: int = 8
+allied_with_united_states: bool = True
 average_inflation_rate: float = 3.3
 services_offered: list = ["Data Analysis", "Python Scripting", "Insider trading"]
-inflation_of_countries: list = [4.8, 4.6, 4.9, 5.0, 4.7, 12.1, 4.9]
+inflation_of_countries: list = [4.2, 2.1, 3.9, 5.0, 4.7, 12.1, 4.9, 7.5]
 
-#Use f-strings to create formatted strings for non-string variables. For example:
+
+#Use f-strings to create formatted strings for non-string variables.
 
 current_customers_string: str = f"Active Projects: {current_customers}"
-trades_with_united_states_string: str = f"Trade with the United Stated: {trades_with_united_states}"
+allied_with_united_states_string: str = f"Trade with the United Stated: {allied_with_united_states}"
 average_inflation_rate_string: str = f"Average Inflation Rate: {average_inflation_rate}"
+inflation_of_countries_string: str = f"Inflation list: {inflation_of_countries}"
+services_offered_string: str = f"Services offered: {services_offered}"
 
 
-"""Use built-in Python functions and the statistics module to calculate descriptive statistics for your numeric list. 
-Python has built-in functions for calculating min(), max(), sum(), and len(). The statistics module provides additional functions for mean(), mode(), median(), and stdev()."""
-
-
-smallest= min(inflation_of_countries)
-largest= max(inflation_of_countries)
-total= sum(inflation_of_countries)
-count= len(inflation_of_countries)
-mean= statistics.mean(inflation_of_countries)
-mode= statistics.mode(inflation_of_countries)
-median= statistics.median(inflation_of_countries)
-standard_deviation=statistics.stdev(inflation_of_countries)
+#Statistics for numeric list. 
+smallest = min(inflation_of_countries)
+largest = max(inflation_of_countries)
+total = sum(inflation_of_countries)
+count = len(inflation_of_countries)
+mean = statistics.mean(inflation_of_countries)
+mode = statistics.mode(inflation_of_countries)
+median = statistics.median(inflation_of_countries)
+standard_deviation = statistics.stdev(inflation_of_countries)
 
 stats_string: str = f"""
 Descriptive Statistics for European Country's inflation rate:
@@ -44,36 +41,30 @@ Descriptive Statistics for European Country's inflation rate:
   Mode: {mode}
   Median: {median}
   Standard Deviation: {standard_deviation}
-
-  6. Define Byline String
-Create a multiline string to display your formatted information. For example:
 """
 
+#Define the byline string.
 byline: str = f"""
-Company Name: {company_name}
-Current customers: {current_customers}
-Trades with USA: {international_presence_string}
-{client_satisfaction_string}
+{company_name}
+{current_customers_string}
+{allied_with_united_states_string}
 {services_offered_string}
 {stats_string}
 """
 
-Define a main() function that we can call to test our code and display all information.
 
+#Call main() function to test code
 def main():
     ''' Display all output'''
     print(company_name)
-    print(active_projects_string)
-    print(international_presence_string)
-    print(client_satisfaction_string)
+    print(current_customers_string)
+    print(allied_with_united_states_string)
     print(services_offered_string)
-    print(numbers_string)
+    print(average_inflation_rate_string)
     print(stats_string)
-
-    # If all of the above works, then the byline should work too:
+  
     print(byline)
 
-
-  if __name__ == '__main__':
-    main()
+if __name__ == '__main__':
+  main()
 
